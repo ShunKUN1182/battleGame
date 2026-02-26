ce27b47355e870cb494c22b88d23b7325c0a6e0a
ShunKUN1182 <2250378@ecc.ac.jp>
2026-02-26 11:34:53 +0900

    HPを減らす作業を作る

--- pokemonBattle/js/index.js
+++ pokemonBattle/js/index.js
@@ -1,11 +1,11 @@
-console.log("foo");
-
+const startBtn = document.querySelector("#startBtn");
 const myPokemons = [
     {
         Name: "ピカチュウ",
         HP: 100,
-        ATK: 60,
+        ATK: 70,
         DEF: 50,
+        Speed: 90,
         Type: "でんき",
     },
     {
@@ -13,16 +13,17 @@ const myPokemons = [
         HP: 100,
         ATK: 60,
         DEF: 50,
+        Speed: 40,
         Type: "ほのお",
     },
 ];
-
 const cpuPokemons = [
     {
         Name: "ドガース",
         HP: 100,
         ATK: 60,
-        DEF: 50,
+        DEF: 40,
+        Speed: 20,
         Type: "あく",
     },
     {
@@ -30,8 +31,21 @@ const cpuPokemons = [
         HP: 100,
         ATK: 60,
         DEF: 50,
+        Speed: 60,
         Type: "みず",
     },
 ];
 
-console.log(myPokemons, cpuPokemons);
+startBtn.addEventListener("click", () => {
+    console.log(myPokemons[0].Speed);
+    const myPickPokemon = myPokemons[0];
+    const cpuPickPokemon = cpuPokemons[0];
+    if (myPickPokemon.Speed >= cpuPickPokemon.Speed) {
+        console.log("私の攻撃");
+        cpuPickPokemon.HP = cpuPickPokemon.HP - (myPickPokemon.ATK - cpuPickPokemon.DEF);
+        console.log("残りHP:", cpuPickPokemon.HP);
+    } else {
+        console.log("相手の攻撃");
+        myPickPokemon.HP = myPickPokemon.HP - (cpuPickPokemon.ATK - myPickPokemon.DEF);
+    }
+});
