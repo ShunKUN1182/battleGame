e2382018ef86085a86e1dc7d733418be9ae26502
ShunKUN1182 <2250378@ecc.ac.jp>
2026-02-26 11:20:37 +0900

    自分のポケモンと相手のポケモンを用意

---
+++ pokemonBattle/js/index.js
@@ -0,0 +1,37 @@
+console.log("foo");
+
+const myPokemons = [
+    {
+        Name: "ピカチュウ",
+        HP: 100,
+        ATK: 60,
+        DEF: 50,
+        Type: "でんき",
+    },
+    {
+        Name: "リザードン",
+        HP: 100,
+        ATK: 60,
+        DEF: 50,
+        Type: "ほのお",
+    },
+];
+
+const cpuPokemons = [
+    {
+        Name: "ドガース",
+        HP: 100,
+        ATK: 60,
+        DEF: 50,
+        Type: "あく",
+    },
+    {
+        Name: "カメックス",
+        HP: 100,
+        ATK: 60,
+        DEF: 50,
+        Type: "みず",
+    },
+];
+
+console.log(myPokemons, cpuPokemons);
