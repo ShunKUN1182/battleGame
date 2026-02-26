e8415e0f1134c19e3a1301cd49ce807b84c5d07e
ShunKUN1182 <2250378@ecc.ac.jp>
2026-02-26 14:39:46 +0900

    色々ロジック書いてみた

--- pokemonBattle/js/index.js
+++ pokemonBattle/js/index.js
@@ -1,17 +1,17 @@
-const myStatus = {
+let myStatus = {
     Name: "しゅんや",
+    HP: 120,
     ATK: 100,
     DEF: 50,
     SPD: 45,
-    HP: 120,
 };
 
-const cpuStatus = {
+let cpuStatus = {
     Name: "とわ",
+    HP: 90,
     ATK: 140,
     DEF: 30,
     SPD: 70,
-    HP: 90,
 };
 
 const myName = document.querySelector("#myName");
@@ -21,6 +21,12 @@ const cpuStartHP = document.querySelector("#cpuStartHP");
 const battleLog = document.querySelector("#battleLog");
 const myBtn = document.querySelector("#my_atk_btn");
 const cpuBtn = document.querySelector("#cpu_atk_btn");
+const turnName = document.querySelector("#turnName");
+const turnIndex = document.querySelector("#turnIndex");
+const myHPbar = document.querySelector("#myHPbar");
+const cpuHPbar = document.querySelector("#cpuHPbar");
+const myHPIndex = document.querySelector("#myHPIndex");
+const cpuHPIndex = document.querySelector("#cpuHPIndex");
 
 function inputStatus(my, cpu, log = "何も起こらなかった！！") {
     myName.innerHTML = my.Name;
@@ -31,3 +37,22 @@ function inputStatus(my, cpu, log = "何も起こらなかった！！") {
 }
 
 inputStatus(myStatus, cpuStatus);
+
+myBtn.addEventListener("click", () => {
+    const HPpercent = (HPcheck(myStatus, cpuStatus) / cpuStatus.HP) * 100;
+    cpuStatus.HP = HPcheck(myStatus, cpuStatus);
+    cpuHPbar.style.width = `${HPpercent}%`;
+    cpuHPIndex.textContent = `${HPcheck(myStatus, cpuStatus)}`;
+});
+
+function HPcheck(ATkside, DEFside) {
+    let damage = ATkside.ATK - DEFside.DEF;
+    if (damage <= 0) {
+        damage = 0;
+    }
+    let NewHP = DEFside.HP - damage;
+    if (NewHP <= 0) {
+        NewHP = 0;
+    }
+    return NewHP;
+}
