5094b630dc43e7957d1188d6e305747adedfa1ff
ShunKUN1182 <2250378@ecc.ac.jp>
2026-02-26 13:56:47 +0900

    初期ステータスを画面に表示

--- pokemonBattle/js/index.js
+++ pokemonBattle/js/index.js
@@ -1,56 +1,29 @@
-const startBtn = document.querySelector("#startBtn");
-const myPokemons = [
-    {
-        Name: "ピカチュウ",
-        HP: 100,
-        ATK: 70,
-        DEF: 50,
-        Speed: 90,
-        Type: "でんき",
-    },
-    {
-        Name: "リザードン",
-        HP: 100,
-        ATK: 60,
-        DEF: 50,
-        Speed: 40,
-        Type: "ほのお",
-    },
-];
-const cpuPokemons = [
-    {
-        Name: "ドガース",
-        HP: 100,
-        ATK: 60,
-        DEF: 40,
-        Speed: 20,
-        Type: "あく",
-    },
-    {
-        Name: "カメックス",
-        HP: 100,
-        ATK: 60,
-        DEF: 50,
-        Speed: 60,
-        Type: "みず",
-    },
-];
+const myStatus = {
+    Name: "しゅんや",
+    ATK: 100,
+    DEF: 50,
+    SPD: 45,
+    HP: 120,
+};
 
-startBtn.addEventListener("click", () => {
-    console.log(myPokemons[0].Speed);
-    const myPickPokemon = myPokemons[0];
-    const cpuPickPokemon = cpuPokemons[0];
-    if (myPickPokemon.Speed >= cpuPickPokemon.Speed) {
-        console.log("私の攻撃");
-        cpuPickPokemon.HP = HPCheck(myPickPokemon, cpuPickPokemon);
-        console.log(cpuPickPokemon);
-    } else {
-        console.log("相手の攻撃");
-        myPickPokemon.HP = myPickPokemon.HP - (cpuPickPokemon.ATK - myPickPokemon.DEF);
-    }
-});
+const cpuStatus = {
+    Name: "とわ",
+    ATK: 140,
+    DEF: 30,
+    SPD: 70,
+    HP: 90,
+};
 
-function HPCheck(ATkside, DFEside) {
-    DFEside.HP = DFEside.HP - (ATkside.ATK - DFEside.DEF);
-    return DFEside.HP;
+const myName = document.querySelector("#myName");
+const cpuName = document.querySelector("#cpuName");
+const myStartHP = document.querySelector("#myStartHP");
+const cpuStartHP = document.querySelector("#cpuStartHP");
+
+function inputStatus(my, cpu) {
+    myName.innerHTML = my.Name;
+    cpuName.innerHTML = cpu.Name;
+    myStartHP.innerHTML = `<span>${my.HP}</span>/${my.HP}`;
+    cpuStartHP.innerHTML = `<span>${cpu.HP}</span>/${cpu.HP}`;
 }
+
+inputStatus(myStatus, cpuStatus);
